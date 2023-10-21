a1580e353ca8ccd4c993183eb42554e00eea0d17
wally_yoon <bluegmlduf2@naver.com>
2023-10-21 15:09:35 +0900

    add : initial design update

--- src/app.vue
+++ src/app.vue
@@ -7,9 +7,7 @@ const locale = useState<string>('locale.i18n')
 
 <template>
   <Html :lang="locale">
-    <Body
-      class="bg-white text-gray-800 antialiased transition-colors duration-300 dark:bg-gray-900 dark:text-gray-200"
-    >
+    <Body>
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,68 +8,73 @@ const lists = computed<ILink[]>(() => [
   { id: 1, title: t('modal'), url: 'path1/modal', icon: 'carbon:circle-solid' },
   { id: 1, title: 'Menu', url: 'path1/menu', icon: 'carbon:circle-solid' },
 ])
+// TODO nav추가 => 번역기능 , 입력템플릿, 검색기능, 페이지네이션, 메일전송기능
 </script>
 
 <template>
-  <div class="mx-auto mt-4 max-w-sm">
-    <div class="mb-10 flex items-center justify-between">
-      <a target="_blank" href="#">TEST</a>
-      <div class="flex space-x-2">
-        <MoleculesLanguageChange />
+  <main class="bg-white dark:bg-gray-900 max-w-screen-md mx-auto">
+    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div class="md:flex md:justify-between md:items-center font-light text-gray-500 sm:text-lg dark:text-gray-400 pb-4 border-b-2">
+        <h2 class="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          Thank you for ~
+        </h2>
+        <p class="text-xl mt-2 md:mt-0">
+          ~해 주셔서 감사합니다
+        </p>
       </div>
+      <h3 class="text-2xl font-bold py-3">
+        Useful Expression
+      </h3>
+      <ol class="space-y-1">
+        <li class="flex justify-between">
+          <span>Thank you for Calling</span>
+          <span>전화해주셔서감사합니다</span>
+        </li>
+        <li class="flex justify-between">
+          <span>Thank you for Waiting</span>
+          <span>기다려주셔서 감사합니다</span>
+        </li>
+        <li class="flex justify-between">
+          <span>Thank you for Your trouble</span>
+          <span>애써 주셔서 감사합니다</span>
+        </li>
+      </ol>
+      <h3 class="text-2xl font-bold py-3 mt-2">
+        Dialogue
+      </h3>
+      <ul class="space-y-1">
+        <li>
+          <span>A :</span>
+          <span>Thank you for joining us</span>
+        </li>
+        <li>
+          <span>B :</span>
+          <span>Thank you for inviting us</span>
+        </li>
+      </ul>
+      <ul class="space-y-1">
+        <li>
+          <span>A :</span>
+          <span>함께 해 주신 것을 환영합니다</span>
+        </li>
+        <li>
+          <span>B :</span>
+          <span>초대해 주셔서 감사합니다</span>
+        </li>
+      </ul>
+      <h3 class="text-2xl font-bold py-3 mt-2">
+        Exercises
+      </h3>
+      <ol class="space-y-1">
+        <li class="flex justify-between">
+          <span>Thank you for ____</span>
+          <span>와 주셔서 감사합니다</span>
+        </li>
+        <li class="flex justify-between">
+          <span>Thank you for ____</span>
+          <span>모든것에 감사합니다</span>
+        </li>
+      </ol>
     </div>
-    <div class="flex flex-col">
-      <h1>{{ t("follow") }}</h1>
-      <div class="flex w-full items-center justify-between py-2">
-        <img
-          class="h-20 w-20 rounded-full"
-          height="160"
-          width="160"
-          src="https://www.asia-quest.jp/hubfs/AQJP/AQJP-News/AQlogo-PR.jpg"
-          alt="AQ"
-        >
-
-        <div
-          class="place-items-centerr grid grid-cols-3 justify-items-center gap-4 [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:rounded-md [&>*]:bg-gray-100 [&>*]:p-3 dark:[&>*]:bg-gray-600"
-        >
-          <a
-            target="blank"
-            class="font-bold text-gray-900 hover:bg-gray-300 dark:text-white dark:hover:bg-gray-400"
-            href="https://www.asia-quest.jp/en/"
-          >
-            <Icon name="carbon:link" />
-          </a>
-
-          <a
-            class="hover:bg-gray-300 dark:hover:bg-gray-400"
-            href="#"
-            target="_blank"
-          >
-            <Icon name="carbon:link" />
-          </a>
-
-          <a
-            target="blank"
-            class="font-bold text-gray-900 hover:bg-gray-300 dark:text-white dark:hover:bg-gray-400"
-            href="#"
-          >
-            <Icon name="carbon:link" />
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <ul role="list" class="mt-10 space-y-4">
-      <li v-for="item in lists" :key="item.id">
-        <AtomsList
-          :link="{
-            id: item.id,
-            title: item.title,
-            url: item.url,
-            icon: item.icon,
-          }"
-        />
-      </li>
-    </ul>
-  </div>
+  </main>
 </template>
